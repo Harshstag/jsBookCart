2ad7c5d424ff2b753211e25f5e9bb2015d500fea
Harsh Singh <62803431+Harshstag@users.noreply.github.com>
2024-03-07 13:59:16 +0530

    main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const weatherAPIKey = "9a1098d952395236e30282037540e12e";
+const weatherAPIKey = "9a1098d952395236e3028203754XXXXX";
 
 const weatherAPIURL = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
 
